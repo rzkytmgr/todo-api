da9326abf626d48c5ce3bd2780bd0d1e437130ea
rzkytmgr <rzkytmgr@gmail.com>
2022-09-18 19:33:13 +0700

    :bug: rename env

--- config.app.js
+++ config.app.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 module.exports = {
   database: {
     dialect: 'mysql',
-    name: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
+    name: process.env.MYSQL_DBNAME,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || 3306,
   },
   server: {
     host: 'http://localhost',
